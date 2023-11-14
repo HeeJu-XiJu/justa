bbf84509cb7e1c9611d1e8ac523831e65a0f12fc
hhjjoa2817 <hhjjoa2817@naver.com>
2023-11-14 17:47:48 +0900

    Login 기능 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'profile', )
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass
